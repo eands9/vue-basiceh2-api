182094c5581e85997e2198d0ce627f1d69676e97
eands9 <eands9@yahoo.com>
2022-07-21 14:47:46 -0500

    added sendmail

--- api/sendmail/index.js
+++ api/sendmail/index.js
@@ -1,13 +1,51 @@
+const nodemailer = require("nodemailer");
+// const express=require('express');
+// const app=express();
+
+// app.use(express.static('public'));
+// app.use(express.json())
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    let transporter = nodemailer.createTransport({
+        service: "hotmail",
+        // host: "smtp.gmail.com",
+        // port: 465,
+        // secure: true,
+        // service: "gmail",
+        auth: {
+        //   user: process.env.EMAIL,
+        //   pass: process.env.PASSWORD,
+            user: "kffsande@outlook.com",
+            pass: "Pwd4Kff5and3"
+        },
+        });
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "What's up Wewo";
+    //   let mailIDs = await getMailIDs();
+    //   for (i = 0; i < mailIDs.length; i++) {
+    //     await transporter.sendMail({
+    //       from: "abc@gmail.com",
+    //       to: `${mailIDs[i]}`,
+    //       subject: "Test Mail",
+    //       text: {
+    //         path: `emailBody.txt`,
+    //       },
+    //     });
+    //   }
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+        const mailOptions = {
+            from: "kffsande@outlook.com",
+            to: "kffsande@outlook.com",
+            // subject: req.body.name + "'s order is shown below...",
+            subject: "Test Subject 8",
+            text: "Wow! That's Easy"
+        }
+
+        transporter.sendMail(mailOptions,(error, info)=>{
+            if(error){
+                console.log(error);
+                res.send('error');
+            }else{
+                console.log("Sent: " + info.response); 
+            }
+        }) 
+};
